6a5eb767857630e8fc461fbe6f4086bd2c0aa119
Prajil K <76465233+prajilk@users.noreply.github.com>
2024-12-13 17:05:58 +0530

    Update Works.js

--- src/Works/Works.js
+++ src/Works/Works.js
@@ -3,7 +3,7 @@ const workArray = [
         title: 'ACI Voyage ✈️',
         description: "Developed a dynamic website for France-based ACI Voyage using Next.js and GSAP, ensuring smooth animations and a responsive design. Managed domain purchasing, hosting setup, and ongoing maintenance to support their online presence.",
         techStack: ['Next.js', 'GSAP', 'TailwindCSS'],
-        siteLink: 'https://fluxora-aci.vercel.app',
+        siteLink: 'https://aci-voyage.com',
         imgPath: 'fluxora-aci.png',
         scrollable: true
     }
