064dae7b031d069c6e18b8199c49989cb6e9faa4
Prajil K <prajilk20017@gmail.com>
2025-10-19 19:25:26 +0530

    added 2 works

--- src/Works/Works.js
+++ src/Works/Works.js
@@ -1,5 +1,21 @@
 const workArray = [
         {
+                title: 'Rankinmeta 💼',
+                description: "Developed a corporate website for a Dubai-based B2B marketing agency using Next.js and Strapi, focusing on performance, SEO, and easy content management.",
+                techStack: ['Next.js', 'Strapi CMS', 'TailwindCSS', 'GSAP'],
+                siteLink: 'https://rankinmeta.com',
+                imgPath: 'rankinmeta.webp',
+                scrollable: true
+        },
+        {
+                title: 'Al Shabab Tours ✈️',
+                description: "Developed a website for a Dubai-based travel agency using Next.js and Strapi, ensuring responsive design, smooth content management, and reliable hosting.",
+                techStack: ['Next.js', 'Strapi CMS', 'TailwindCSS'],
+                siteLink: 'https://alshababtours.ae',
+                imgPath: 'alshababtours.webp',
+                scrollable: true
+        },
+        {
                 title: 'ACI Voyage ✈️',
                 description: "Developed a dynamic website for France-based ACI Voyage using Next.js and GSAP, ensuring smooth animations and a responsive design. Managed domain purchasing, hosting setup, and ongoing maintenance to support their online presence.",
                 techStack: ['Next.js', 'GSAP', 'TailwindCSS'],
